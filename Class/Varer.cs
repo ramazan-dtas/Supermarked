f5cf1ffd56225c2bdc716e5fd932a0c8008c873e
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-11-03 12:52:19 +0100

    Vare tilføjet

--- Class/Varer.cs
+++ Class/Varer.cs
@@ -8,9 +8,9 @@ namespace Supermarked.Class
 {
     public class Varer
     {
-        public int number { get; set; }
+        public int number { get;  set; }
         public string name { get; set; }
-        public int price { get; set; }
+        public double price { get; set; }
         /*
         public Varer(int Number, string Name, int Price)
         {
@@ -19,9 +19,51 @@ namespace Supermarked.Class
             price = Price;
         }*/
 
-        public void vareads()
+        public void vare(int Number, string Name, double Price)
         {
+            number = Number;
+            name = Name;
+            price = Price;
 
+            Name = funktionalitet.Select(new string[] { "Coca Cola", "Fanta", "Sødmælk", "Letmælk", "Løg", "Agurk", "Chips", "Slik", "Gå til kassen"}).ToLower();
+
+            switch (Name)
+            {
+                case "Coca Cola":
+                    Number = 132456;
+                    Price = 15.00;
+                    break;
+                case "Fanta":
+                    Number = 123489;
+                    Price = 15.00;
+                    break;
+                case "Sødmælk":
+                    Number = 421429;
+                    Price = 7.95;
+                    break;
+                case "Letmælk":
+                    Number = 789201;
+                    Price = 7.50;
+                    break;
+                case "Løg":
+                    Number = 647892;
+                    Price = 5.00;
+                    break;
+                case "Agurk":
+                    Number = 782345;
+                    Price = 9.00;
+                    break;
+                case "Chips":
+                    Number = 723456;
+                    Price = 18.32;
+                    break;
+                case "Slik":
+                    Number = 045678;
+                    Price = 33.21;
+                    break;
+                case "Gå til kassen":
+                    break;
+            }
         }
     }
 }
