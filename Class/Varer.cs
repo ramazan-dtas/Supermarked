2d6fbdf9c696e6748d4d995c8f7ed83f788cd52a
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-11-05 14:13:47 +0100

    Loop tilføjet

--- Class/Varer.cs
+++ Class/Varer.cs
@@ -7,61 +7,70 @@ using System.Threading.Tasks;
 namespace Supermarked.Class
 {
     public class Varer
-    {//De her tre tager 
+    {//De her tre tager værdierne fra varer og sætter dem ud på output
         public int number { get; set; }
         public string name { get; set; }
         public double price { get; set; }
 
         public void varer(int Number, string Name, double Price)
         {
-            Name = funktionalitet.Select(new string[] { "Coca Cola", "Fanta", "Sødmælk", "Letmælk", "Løg", "Agurk", "Chips", "Slik", "Gå til kassen" });
-            Console.Clear();
-            switch (Name)
+            //Her 
+            bool sa = false;
+            while (!sa)
             {
-                case "Coca Cola":
-                    Number = 132456;
-                    Price = 15.00;
-                    break;
+                Name = funktionalitet.Select(new string[] { "Coca Cola", "Fanta", "Sødmælk", "Letmælk", "Løg", "Agurk", "Chips", "Slik", "Gå til kassen" });
+                Console.Clear();
+                switch (Name)
+                {
+                    case "Coca Cola":
+                        Number = 132456;
+                        Price = 15.00;
+                        break;
 
-                case "Fanta":
-                    Number = 123489;
-                    Price = 15.00;
-                    break;
+                    case "Fanta":
+                        Number = 123489;
+                        Price = 15.00;
+                        break;
 
-                case "Sødmælk":
-                    Number = 421429;
-                    Price = 7.95;
-                    break;
+                    case "Sødmælk":
+                        Number = 421429;
+                        Price = 7.95;
+                        break;
 
-                case "Letmælk":
-                    Number = 789201;
-                    Price = 7.50;
-                    break;
+                    case "Letmælk":
+                        Number = 789201;
+                        Price = 7.50;
+                        break;
 
-                case "Løg":
-                    Number = 647892;
-                    Price = 5.00;
-                    break;
+                    case "Løg":
+                        Number = 647892;
+                        Price = 5.00;
+                        break;
 
-                case "Agurk":
-                    Number = 782345;
-                    Price = 9.00;
-                    break;
+                    case "Agurk":
+                        Number = 782345;
+                        Price = 9.00;
+                        break;
 
-                case "Chips":
-                    Number = 723456;
-                    Price = 18.32;
-                    break;
+                    case "Chips":
+                        Number = 723456;
+                        Price = 18.32;
+                        break;
 
-                case "Slik":
-                    Number = 045678;
-                    Price = 33.21;
-                    break;
+                    case "Slik":
+                        Number = 045678;
+                        Price = 33.21;
+                        break;
 
-                case "Gå til kassen":
-                    new Betaling();
-                    break;
+                    case "Gå til kassen":
+                        sa = true;
+                        Console.Clear();
+                        new Betaling();
+                        break;
+
+                }
             }
+           
            
             name = Name;
             number = Number;
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Supermarked
             Class.Varer p1 = new Class.Varer();
             p1.varer(p1.number,p1.name,p1.price);
             Console.WriteLine($"Vare navn {p1.name} nummer {p1.number.ToString()} pris {p1.price}");
-
+            
             Console.ReadKey();
         }
     }
