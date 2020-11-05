ae7916633a2a32e30bc2cc262fff316ae5b7259b
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-11-05 12:41:10 +0100

    Betalingsmiddel add

--- Class/Betaling.cs
+++ Class/Betaling.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace Supermarked.Class
 {
-    public class Betaling
+    public class Betaling: Varer
     {
         public string betalingsmiddel { get; set; }
-        public int Pris { get; set; }
+        public int totalpris { get; set; }
 
+        public void betal(string Betalingsmiddel, int Totalpris)
+        {
+
+            Betalingsmiddel = funktionalitet.Select(new string[] { "Kontant", "Dankort" });
+            Console.Clear();
+            switch (Betalingsmiddel)
+            {
+                case "Kontant":
+                    break;
+                case "Dankort":
+                    break;
+                default:
+                    break;
+            }
+
+            if(Betalingsmiddel == "Kontant")
+            {
+                bool pay = true;
+                while (pay)
+                {
+                    Betalingsmiddel = funktionalitet.Select(new string[] { "50", "100", "200", "500", "1000" });
+                    Console.Clear();
+                    switch (Betalingsmiddel)
+                    {
+                        case "50":
+                            break;
+                        case "100":
+                            break;
+                        case "200":
+                            break;
+                        case "500":
+                            break;
+                        case "1000":
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            }
+            betalingsmiddel = Betalingsmiddel;
+            totalpris = Totalpris;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace Supermarked
         static void Main(string[] args)
         {
             Class.Varer p1 = new Class.Varer();
-            
             p1.varer(p1.number,p1.name,p1.price);
             Console.WriteLine($"Vare navn {p1.name} nummer {p1.number.ToString()} pris {p1.price}");
+
             Console.ReadKey();
         }
     }
