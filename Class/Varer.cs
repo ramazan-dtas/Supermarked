cb19313d7e1016aae6bd551ff23c516da64e170c
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-11-05 10:35:18 +0100

    varer done update

--- Class/Varer.cs
+++ Class/Varer.cs
@@ -25,51 +25,43 @@ namespace Supermarked.Class
                 case "Coca Cola":
                     Number = 132456;
                     Price = 15.00;
-
-                    //Console.Write(Number.ToString(), Price);
                     break;
+
                 case "Fanta":
                     Number = 123489;
                     Price = 15.00;
-                    //Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Sødmælk":
                     Number = 421429;
                     Price = 7.95;
-                    //Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Letmælk":
                     Number = 789201;
                     Price = 7.50;
-                    //Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Løg":
                     Number = 647892;
                     Price = 5.00;
- //                   Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Agurk":
                     Number = 782345;
                     Price = 9.00;
-   //                 Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Chips":
                     Number = 723456;
                     Price = 18.32;
-     //               Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Slik":
                     Number = 045678;
                     Price = 33.21;
-       //             Console.Write(Number.ToString(), Price);
-
                     break;
+
                 case "Gå til kassen":
                     new Betaling();
                     break;
